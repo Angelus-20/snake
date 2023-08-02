c23cc363bcbdb4e2b0678cf8e0971b17e228f914
Angelus-20 <edwinalexander20@gmail.com>
2023-08-02 19:29:28 -0400

    update input info

--- client.js
+++ client.js
@@ -23,6 +23,26 @@ const connect = function () {
     console.log(`Received data: ${data}`);
   });
 
+  const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+
+    stdin.on("data", handleUserInput);
+
+    return stdin;
+  };
+
+  const handleUserInput = function (data) {
+    if (data === '\u0003') {
+      process.exit(); // Exit on Ctrl+C
+    }
+    // Add other conditions to handle other input, if needed
+  };
+
+  setupInput();
+
   return conn;
 };
 
--- play.js
+++ play.js
@@ -1,19 +1,6 @@
-const net = require("net");
+const { setupInput } = require('./input');
+const connect = require('./client');
 
-// // establishes a connection with the game server
-// const connect = function () {
-//   const conn = net.createConnection({
-//     host: '165.227.47.243',
-//     port: 50541
-//   });
-
-//   // interpret incoming data as text
-//   conn.setEncoding("utf8");
-
-//   return conn;
-// };
-
-// console.log("Connecting ...");
-// connect();
-
-module.exports = connect;
+console.log("Connecting ...");
+connect();
+setupInput();
