c3aed7fa5369cfbc778ba95f9c7722ce739a918f
Angelus-20 <edwinalexander20@gmail.com>
2023-08-02 17:19:36 -0400

    update info

--- play.js
+++ play.js
@@ -1,17 +1,19 @@
 const net = require("net");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
-  });
+// // establishes a connection with the game server
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: '165.227.47.243',
+//     port: 50541
+//   });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
 
-  return conn;
-};
+//   return conn;
+// };
 
-console.log("Connecting ...");
-connect();
+// console.log("Connecting ...");
+// connect();
+
+module.exports = connect;
